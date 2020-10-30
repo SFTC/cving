53b2fc3489d3549260ebef1e753854a88f48500e
songwenhui <songwen1073@126.com>
2020-10-30 16:30:52 +0800

    feature：FormSpan

---
+++ demos/formspan/FormSpan.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export interface FormSpanProps {
+  value?: string;
+  name?: string;
+  onChange?: (v: string) => {};
+  style?: React.CSSProperties;
+  className?: string;
+}
+/** 用于表单内固定展示，却需要提交时带入该值的项 */
+const FormSpan: React.FC<FormSpanProps> = props => {
+  const { value, style, className } = props;
+
+  return (
+    <span className={className} style={style}>
+      {value}
+    </span>
+  );
+};
+
+export default FormSpan;
